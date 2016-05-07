06ab9db5a6787c263417b0dbf5e0aa8ad6e1c7a7
Oleg Sushkov <osushkov@gmail.com>
2016-05-07 17:51:38 +1000

    finished the gradient kernel and a CPU implementation of feed forward, now time for testing

--- src/neuralnetwork/Network.cpp
+++ src/neuralnetwork/Network.cpp
@@ -1,8 +1,11 @@
 
 #include "Network.hpp"
+#include "../common/Util.hpp"
 #include "../math/Tensor.hpp"
 #include "Activations.hpp"
 #include <cassert>
+#include <cmath>
+#include <cstdlib>
 
 using namespace neuralnetwork;
 
@@ -12,42 +15,98 @@ struct Network::NetworkImpl {
 
   NetworkImpl(const NetworkSpec &spec) : spec(spec) {
     assert(spec.numInputs > 0 && spec.numOutputs > 0);
+    initialiseWeights();
   }
 
   EVector Process(const EVector &input) {
     assert(input.rows() == spec.numInputs);
 
-    return input;
+    EVector layerOutput = input;
+    for (unsigned i = 0; i < layerWeights.NumLayers(); i++) {
+      LayerActivation func =
+          (i == layerWeights.NumLayers() - 1) ? spec.outputActivation : spec.hiddenActivation;
+      layerOutput = getLayerOutput(layerOutput, layerWeights(i), func);
+    }
+
+    return layerOutput;
   }
 
   void Update(const SamplesProvider &samplesProvider) {}
 
-  // Returns the output vector of the layer, and the derivative vector for the layer.
-  pair<EVector, EVector> getLayerOutput(const EVector &prevLayer, const EMatrix &layerWeights,
-                                        LayerActivation afunc) const {
+  EVector getLayerOutput(const EVector &prevLayer, const EMatrix &layerWeights,
+                         LayerActivation afunc) const {
+    assert(prevLayer.rows() == layerWeights.cols() - 1);
+
     EVector z = layerWeights * getInputWithBias(prevLayer);
-    // layerWeights.topRightCorner(layerWeights.rows(), layerWeights.cols() - 1) * prevLayer;
-    EVector derivatives(z.rows());
+    if (afunc == LayerActivation::SOFTMAX) {
+      z = softmaxActivations(z);
+    } else {
+      for (unsigned i = 0; i < z.rows(); i++) {
+        z(i) = ActivationValue(afunc, z(i));
+      }
+    }
 
-    // if (isOutput && useSoftmax) {
-    //   z = Util::SoftmaxActivations(z);
-    // } else {
-    //   for (unsigned i = 0; i < layerWeights.rows(); i++) {
-    //     // z(i) += layerWeights(i, 0);
-    //     float in = z(i);
-    //
-    //     z(i) = func->ActivationValue(in);
-    //     derivatives(i) = func->DerivativeValue(in, z(i));
-    //   }
-    // }
-
-    return make_pair(z, derivatives);
+    return z;
   }
 
   EVector getInputWithBias(const EVector &noBiasInput) const {
     EVector result(noBiasInput.rows() + 1);
     result(noBiasInput.rows()) = 1.0f;
     result.topRightCorner(noBiasInput.rows(), 1) = noBiasInput;
+    return result;
+  }
+
+  void initialiseWeights(void) {
+    if (spec.hiddenLayers.empty()) {
+      layerWeights.AddLayer(createLayer(spec.numInputs, spec.numOutputs));
+    } else {
+      layerWeights.AddLayer(createLayer(spec.numInputs, spec.hiddenLayers[0]));
+
+      for (unsigned i = 0; i < spec.hiddenLayers.size() - 1; i++) {
+        layerWeights.AddLayer(createLayer(spec.hiddenLayers[i], spec.hiddenLayers[i + 1]));
+      }
+
+      layerWeights.AddLayer(
+          createLayer(spec.hiddenLayers[spec.hiddenLayers.size() - 1], spec.numOutputs));
+    }
+  }
+
+  EMatrix createLayer(unsigned inputSize, unsigned layerSize) const {
+    assert(inputSize > 0 && layerSize > 0);
+
+    unsigned numRows = layerSize;
+    unsigned numCols = inputSize + 1; // +1 accounts for bias input
+    float initRange = 1.0f / sqrtf(numCols);
+
+    EMatrix result(numRows, numCols);
+    for (unsigned r = 0; r < result.rows(); r++) {
+      for (unsigned c = 0; c < result.cols(); c++) {
+        result(r, c) = Util::RandInterval(-initRange, initRange);
+      }
+    }
+
+    return result;
+  }
+
+  EVector softmaxActivations(const EVector &in) const {
+    assert(in.rows() > 0);
+    EVector result(in.rows());
+
+    float maxVal = in(0);
+    for (int r = 0; r < in.rows(); r++) {
+      maxVal = fmax(maxVal, in(r));
+    }
+
+    float sum = 0.0f;
+    for (int i = 0; i < in.rows(); i++) {
+      result(i) = expf(in(i)-maxVal);
+      sum += result(i);
+    }
+
+    for (int i = 0; i < result.rows(); i++) {
+      result(i) /= sum;
+    }
+
     return result;
   }
 };
--- src/neuralnetwork/cuda/GradientKernel.cu
+++ src/neuralnetwork/cuda/GradientKernel.cu
@@ -7,6 +7,52 @@
 using namespace neuralnetwork;
 using namespace neuralnetwork::cuda;
 
+__global__ void gradientKernel(LayerBatchDeltas layerDeltas, LayerBatchOutputs layerOutputs,
+                               LayerWeights outGradient) {
+  extern __shared__ float buf[]; // shared memory buffer
+
+  const unsigned row = blockDim.y * blockIdx.y + threadIdx.y;
+  const unsigned col = blockDim.x * blockIdx.x + threadIdx.x;
+
+  // buffer for holding the layer weight matrix chunk
+  float *ldChunk = (float *) buf;
+
+  // buffer for holding the prev outputs matrix chunk
+  float *loChunk = (float *) &buf[blockDim.x * blockDim.y];
+
+  const int ldCol = blockDim.y * blockIdx.y + threadIdx.x;
+  const int loCol = col;
+
+  const int numChunks = (layerDeltas.batchSize + blockDim.y - 1) / blockDim.y;
+  const int chunkIndex = threadIdx.x + threadIdx.y * blockDim.x;
+  const int lim = numChunks * blockDim.y;
+
+  float sum = 0.0f;
+  for (int chunkOffset = 0; chunkOffset < lim; chunkOffset += blockDim.y) {
+    const int ldRow = chunkOffset + threadIdx.y;
+    if (ldRow < layerDeltas.batchSize && ldCol < layerDeltas.layerSize) {
+      ldChunk[chunkIndex] = *layerDeltas.Elem(ldRow, ldCol);
+    }
+
+    const int loRow = ldRow;
+    if (loRow < layerOutputs.batchSize && loCol < layerOutputs.layerSize) {
+      loChunk[chunkIndex] = *layerOutputs.OutputElem(loRow, loCol);
+    }
+    __syncthreads();
+
+    int chunkLim = min(blockDim.x, layerDeltas.batchSize - chunkOffset);
+    for (int j = 0; j < chunkLim; j++) {
+      sum += ldChunk[threadIdx.y + j * blockDim.x] * loChunk[threadIdx.x + j * blockDim.x];
+    }
+
+    __syncthreads();
+  }
+
+  if (row < outGradient.layerSize && col < outGradient.inputSize) {
+    *outGradient.Elem(row, col) = sum / layerDeltas.batchSize;
+  }
+}
+
 void GradientKernel::Apply(LayerBatchDeltas layerDeltas, LayerBatchOutputs layerOutputs,
                            LayerWeights outGradient) {
 
@@ -14,6 +60,10 @@ void GradientKernel::Apply(LayerBatchDeltas layerDeltas, LayerBatchOutputs layer
   assert(layerDeltas.layerSize == outGradient.layerSize);
   assert(layerOutputs.layerSize == outGradient.inputSize);
 
-  // int bpgX = (layerWeights.inputSize + TPB_X - 1) / TPB_X;
-  // int bpgY = (layerWeights.layerSize + TPB_Y - 1) / TPB_Y;
+  int bpgX = (outGradient.inputSize + TPB_X - 1) / TPB_X;
+  int bpgY = (outGradient.layerSize + TPB_Y - 1) / TPB_Y;
+  size_t sharedMemSize = 2 * TPB_X * TPB_Y * sizeof(float);
+
+  gradientKernel<<<dim3(bpgX, bpgY, 1), dim3(TPB_X, TPB_Y, 1), sharedMemSize>>>(
+      layerDeltas, layerOutputs, outGradient);
 }
